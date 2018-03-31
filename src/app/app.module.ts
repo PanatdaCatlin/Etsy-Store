04bdbd2fbc62eae959321256b30a0f3a9c8dc8bc
Panatda Inthala <panatdainthala@gmail.com>
2018-03-30 20:28:36 -0700

    it will have create a new nav bar temple and import firebase modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { routing } from './app.routing';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from './../environments/environment';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
@@ -10,7 +14,14 @@ import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [AppComponent, WelcomeComponent, NavbarComponent],
-  imports: [BrowserModule, FormsModule, HttpModule, routing],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule, routing
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
