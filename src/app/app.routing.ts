7d3e487217d69693cbeba07fa52e938d520ec918
Panatda Inthala <panatdainthala@gmail.com>
2018-04-01 22:34:51 -0700

    it will have change the path and create home component.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,17 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { HomeComponent } from './home/home.component';
 
 
 const appRoutes: Routes = [
   {
-    path: '',
+    path: 'welcome',
     component: WelcomeComponent
+  },
+   {
+    path: '',
+    component: HomeComponent
   }
 ];
 
