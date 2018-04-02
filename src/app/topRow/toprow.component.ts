d6ee2193cf18c44f772f3c73a2b74d5395547847
Panatda Inthala <panatdainthala@gmail.com>
2018-04-01 23:53:19 -0700

    it will include top row in the html include placeholder pictures.

---
+++ src/app/topRow/toprow.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'top-row',
+  templateUrl: './toprow.component.html',
+  styleUrls: ['./toprow.component.css']
+})
+export class TopRowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
