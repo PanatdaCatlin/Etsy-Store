f857d73e75b81c19254ebf8d24df898fa255c3b7
Panatda Inthala <panatdainthala@gmail.com>
2018-04-07 18:20:50 -0700

    it will update top row componet to add a carousel to top row.

--- src/app/topRow/toprow.component.ts
+++ src/app/topRow/toprow.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'top-row',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toprow.component.css']
 })
 export class TopRowComponent implements OnInit {
+  cards = [
+    {
+      src: '.././assets/img/jewelry.jpg',
+      title: 'RAW JEWELRY',
+      sub: 'Find the most imaginative items out there.'
+    },
+    {
+      src: '.././assets/img/peopleandpets.jpg',
+      title: 'CUSTOM MADE',
+      sub: 'Create somthing spectacular with a maker.'
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
