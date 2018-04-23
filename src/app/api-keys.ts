ab4056463b393999bb39d9612ac25e014f0e26e3
Panatda Inthala <panatdainthala@gmail.com>
2018-04-23 11:45:14 -0700

    it will have update formatting

--- src/app/api-keys.ts
+++ src/app/api-keys.ts
@@ -1,8 +1,8 @@
 export const masterFirebaseConfig = {
-    apiKey: "AIzaSyDHwubT1wmSIkXnOjtVCeSp_JhQdCvP_p0",
-    authDomain: "esty-store.firebaseapp.com",
-    databaseURL: "https://esty-store.firebaseio.com",
-    projectId: "esty-store",
-    storageBucket: "esty-store.appspot.com",
-    messagingSenderId: "20104319197"
+  apiKey: 'AIzaSyDHwubT1wmSIkXnOjtVCeSp_JhQdCvP_p0',
+  authDomain: 'esty-store.firebaseapp.com',
+  databaseURL: 'https://esty-store.firebaseio.com',
+  projectId: 'esty-store',
+  storageBucket: 'esty-store.appspot.com',
+  messagingSenderId: '20104319197'
 };
